f364c6a5da9519f8acebb4b3df34700a500e3703
ramarefin <rama.223040040@mail.unpas.ac.id>
2023-02-18 12:22:35 +0700

    first php

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,31 @@
+<?php 
+
+$npm = "40"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php 
+        
+            echo "Aku adalah angka 40 </br>";
+            echo "Jika aku dikali 5, maka aku menjadi ";
+            echo "<b>". $npm * 5 . "</b>" . "</br>";
+            echo "Jika aku dibagi 2, maka aku menjadi ";
+            echo "<b>" . 200 / 2 . "</b>" . "</br>";
+            echo "Jika aku ditambah 75, maka aku akan menjadi ";
+            echo "<b>" . 100 + 75 . "</b></br>";
+            echo "Jika aku dikurang 20, maka aku akan menjadi ";
+            echo "<b>" . 175 - 20 . "</b>";
+        ?>
+    </p>
+</body>
+</html>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+      .kotak2 {
+        display: flex;
+      }
+
+      .kotak3 {
+        display: flex;
+      }
+
+      .box {
+        background-color: aqua;
+        width: 100px;
+        height: 100px;
+        border: 1px solid black;
+        text-align: center;
+        line-height: 100px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="kotak">
+      <div class="box">1</div>
+    </div>
+    <div class="kotak2">
+      <div class="box">2</div>
+      <div class="box">2</div>
+    </div>
+    <div class="kotak3">
+      <div class="box">3</div>
+      <div class="box">3</div>
+      <div class="box">3</div>
+    </div>
+  </body>
+</html>
